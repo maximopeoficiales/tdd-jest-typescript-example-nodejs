c1dfd81c1683df445b9d1461594a6f8e443276f0
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2023-02-20 18:32:10 -0500

    ADD: todos los metodos de la api tienen test

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Endpoint User", () => {
-  it("return user json", async () => {
+  it("GET: return user json", async () => {
     mockedAxios.get.mockResolvedValue({ data: 1 });
     const res = {
       status: jest.fn().mockReturnThis(),
@@ -12,9 +12,54 @@ describe("Endpoint User", () => {
     };
 
     await userController.get({} as any, res as any, {} as any);
-    console.log(res.send.mock.calls);
 
     expect(res.status.mock.calls).toEqual([[200]]);
     expect(res.send.mock.calls).toEqual([[1]]);
+  })
+
+
+  it("POST: create one user return status 201", async () => {
+    mockedAxios.post.mockResolvedValue({ data: 1 });
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    const req = {
+      body: "request body",
+    };
+
+    await userController.post(req as any, res as any, {} as any);
+
+    expect(res.status.mock.calls).toEqual([[201]]);
+    expect(res.send.mock.calls).toEqual([[1]]);
+  })
+
+  it("PUT: create one user return status 204", async () => {
+    mockedAxios.put.mockResolvedValue({ data: 1 });
+    const res = {
+      sendStatus: jest.fn(),
+    };
+    const req = {
+      body: "request body",
+      params: { id: 1 }
+    };
+
+    await userController.put(req as any, res as any, {} as any);
+
+    expect(res.sendStatus.mock.calls).toEqual([[204]]);
+  })
+
+  it("DELETE: delete one user return status 204", async () => {
+    mockedAxios.delete.mockResolvedValue({ data: 1 });
+    const res = {
+      sendStatus: jest.fn(),
+    };
+    const req = {
+      params: { id: 1 }
+    };
+
+    await userController.delete(req as any, res as any, {} as any);
+
+    expect(res.sendStatus.mock.calls).toEqual([[204]]);
   })
 })
